d8dbbe67c1c3a1adbe419f91b4fc6f14d7c0c07c
gurpreet1961 <gurpreet012guru@gmail.com>
2021-11-23 20:28:08 +0530

    new added

--- Assets/Scripts/Wepon/RaCastShot.cs
+++ Assets/Scripts/Wepon/RaCastShot.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class RaCastShot : MonoBehaviour
 {
     public Camera Playercamera;
+    public Transform shootPoint;
     public float FireRate = 10f;
     private float timeBetweenNextShot;
     public float Damage = 20f;
@@ -13,11 +14,19 @@ public class RaCastShot : MonoBehaviour
 
     public mouseLook mouse;
     // public Animator moveEffect;
+    private AudioSource AudioSource;
+    public AudioClip shootSound;
+    public enum ShootMode { Auto, Semi }
+    [Header("ShootMode")]
+    public ShootMode shootingMode;
+    private bool shootInput;
     [Header("Recoil")]
     public float vRecoil = 1f;
     public float hRecoil = 1f;
     public Animator AimAnim;
     public bool aim;
+
+
 
     //Ammo Part
     [Header("Ammo Mangement")]
@@ -25,8 +34,14 @@ public class RaCastShot : MonoBehaviour
     public int availableammo = 100;
     public int maxAmmo = 25;
     public Animator anim;
+
     public Text currentammotext;
 
+    public bool canUse = true;
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
     void Update()
     {
         // if (Input.GetButton("Fire2"))
@@ -37,44 +52,71 @@ public class RaCastShot : MonoBehaviour
         // {
         //     AimAnim.SetBool("Aim", false);
         // }
-        if (Input.GetButtonDown("Fire2"))
-        {
-            aim = !aim;
-        }
-        if (aim == true)
-        {
-            AimAnim.SetBool("Aim", true);
-        }
-        else
-        {
-            AimAnim.SetBool("Aim", false);
-        }
 
-        currentammotext.text = ammocount.ToString();
-        if (Input.GetKeyDown(KeyCode.R) && ammocount < maxAmmo)
+        if (canUse)
         {
-            mouse.AddRecoil(0, 0);
-            anim.SetBool("Reload", true);
-            anim.SetBool("Shoot", false);
+            if (Input.GetButtonDown("Fire2"))
+            {
+                aim = !aim;
+            }
+            if (aim == true)
+            {
+                AimAnim.SetBool("Aim", true);
+            }
+            else
+            {
+                AimAnim.SetBool("Aim", false);
+            }
+            if (canUse)
+            {
+                currentammotext.text = ammocount.ToString();
+            }
+            if (Input.GetKeyDown(KeyCode.R) && ammocount < maxAmmo)
+            {
+                mouse.AddRecoil(0, 0);
+                anim.SetBool("Reload", true);
+                anim.SetBool("Shoot", false);
 
+            }
+            if (ammocount <= 0)
+            {
+                mouse.AddRecoil(0, 0);
+                anim.SetBool("Reload", true);
+                anim.SetBool("Shoot", false);
+
+                return;
+            }
         }
-        if (ammocount <= 0)
+        switch (shootingMode)
         {
-            mouse.AddRecoil(0, 0);
-            anim.SetBool("Reload", true);
-            anim.SetBool("Shoot", false);
+            case ShootMode.Auto:
 
-            return;
+                shootInput = Input.GetButton("Fire1");
+                break;
+            case ShootMode.Semi:
+                shootInput = Input.GetButtonDown("Fire1");
+                break;
         }
-        if (Input.GetButton("Fire1") && Time.time >= timeBetweenNextShot)
+        if (canUse)
         {
-            timeBetweenNextShot = Time.time + 1f / FireRate;
-            float h = Random.Range(-hRecoil, hRecoil);
-            float v = Random.Range(0, vRecoil);
-            anim.SetBool("Shoot", true);
+            if (shootInput && Time.time >= timeBetweenNextShot)
+            {
+                timeBetweenNextShot = Time.time + 1f / FireRate;
+                float h = Random.Range(-hRecoil, hRecoil);
+                float v = Random.Range(0, vRecoil);
+                if (shootInput == Input.GetButtonDown("Fire1"))
+                {
+                    anim.SetBool("Shoot", false);
+                }
+                else
+                {
+                    anim.SetBool("Shoot", true);
+                }
 
-            mouse.AddRecoil(h, v);
-            weapon();
+                mouse.AddRecoil(h, v);
+                weapon();
+
+            }
         }
         if (Input.GetButtonUp("Fire1"))
         {
@@ -83,19 +125,41 @@ public class RaCastShot : MonoBehaviour
 
         }
     }
-    void weapon()
+    public void weapon()
     {
+
         ammocount--;
         RaycastHit hit;
+        PlayShootSound();
         if (Physics.Raycast(Playercamera.transform.position, Playercamera.transform.forward, out hit, range))
         {
-            Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal)); //use to spaw any component(here we are adding hiteffect on ground when bullet hit)
+            GameObject HitSparks = Instantiate(hitEffect, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal)); //use to swap any component(here we are adding hiteffect on ground when bullet hit)
+            Destroy(HitSparks, 2f);
             if (hit.transform.tag == "Enemy")
             {
                 Health enemy = hit.transform.GetComponent<Health>();
                 enemy.Damage(Damage);
             }
 
+        }
+    }
+    public void Enemyweapon()
+    {
+        RaycastHit hit;
+        if (Time.time >= timeBetweenNextShot)
+        {
+            PlayShootSound();
+            timeBetweenNextShot = Time.time + 1f / FireRate;
+            if (Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range))
+            {
+                GameObject HitSparks = Instantiate(hitEffect, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal)); //use to swap any component(here we are adding hiteffect on ground when bullet hit)
+                Destroy(HitSparks, 2f);
+                if (hit.transform.tag == "Player")
+                {
+                    Health enemy = hit.transform.GetComponent<Health>();
+                    enemy.Damage(Damage);
+                }
+            }
         }
     }
     public void Reload()
@@ -106,5 +170,11 @@ public class RaCastShot : MonoBehaviour
         anim.SetBool("Shoot", false);
 
         anim.SetBool("Reload", false);
+    }
+    private void PlayShootSound()
+    {
+        AudioSource.PlayOneShot(shootSound);
+        // AudioSource.clip = shootSound;
+        // AudioSource.Play();
     }
 }
